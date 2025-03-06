38f99975e6a865bf60fcab25f9c5bcb6789229fb
Luciano Larrea <takticflow@gmail.com>
2025-03-06 08:25:27 -0300

    Redirect by navigator language
    Add a redirect.js and switch language Button

---
+++ redirect.js
@@ -0,0 +1,6 @@
+// redirect.js
+if (navigator.language.startsWith("es")) {
+    window.location.href = "https://lucianolarrea.github.io/";
+} else {
+    window.location.href = "https://lucianolarrea.github.io/portfolio/";
+}
